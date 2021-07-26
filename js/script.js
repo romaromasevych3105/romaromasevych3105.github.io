4fffaaf9b32b0313990b5670c86df7bd97a4aeef
romaromasevych3105 <roman.romasevych@gmail.com>
2021-07-26 16:18:38 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector(".hamburger"),
+    menu = document.querySelector(".menu"),
+    closeElem = document.querySelector(".menu__close");
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+const counters = document.querySelectorAll('.skills__exp-percent'),
+    line = document.querySelectorAll('.skills__exp-line span');
+
+counters.forEach((item, i) => {
+    line[i].style.width = item.innerHTML;
+});
