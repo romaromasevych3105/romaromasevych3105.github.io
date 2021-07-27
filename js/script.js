623a088f31efd950ec1c0e7edabe9922d94597bd
romaromasevych3105 <roman.romasevych@gmail.com>
2021-07-27 22:14:01 +0300

    2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 const hamburger = document.querySelector(".hamburger"),
     menu = document.querySelector(".menu"),
-    closeElem = document.querySelector(".menu__close");
+    closeElem = document.querySelector(".menu__close"),
+    link = document.querySelectorAll(".menu__item, .btn, .promo__link");
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
+
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+link.forEach((i) => {
+    i.addEventListener('click', function (event) {
+        event.preventDefault();
+        menu.classList.remove('active');
+        let anchor = document.getElementById(i.dataset.value);
+        anchor.scrollIntoView({
+            behavior: "smooth"
+        });
+
+    });
+});
+
 
 const counters = document.querySelectorAll('.skills__exp-percent'),
     line = document.querySelectorAll('.skills__exp-line span');
